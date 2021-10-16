a3d92a7c38dc79bec29c8bf0eb138708b5f1b05b
JOANITA-51 <nakityojoanita51@gmail.com>
2021-10-16 14:27:23 +0300

    class component

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,10 +1,16 @@
-import Header from "./components/Header";
-function App() {
-  return (
-    <div className="container">
-      <Header/>
-    </div>
-  );
-}
+import React from 'react'
+//import Header from "./components/Header";
+// function App() {
+//   return (
+//     <div className="container">
+//       <Header/>
+//     </div>
+//   );
+// }
 
+class App extends React.Component{
+  render() {
+    return <h1>Hello from a class</h1>
+  }
+}
 export default App;
