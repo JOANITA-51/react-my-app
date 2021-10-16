de000993b3d24a315f6d27a2bd8431c0ac6c2abc
JOANITA-51 <nakityojoanita51@gmail.com>
2021-10-17 02:45:59 +0300

    destructuring the code to make it cleaner

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,9 +1,9 @@
 // import PropTypes from 'prop-types'
 
-const Header = (props) => {
+const Header = ({title}) => {
     return (
         <header>
-            <h1>{props.title}</h1>
+            <h1>{title}</h1>
         </header>
     )
 }
