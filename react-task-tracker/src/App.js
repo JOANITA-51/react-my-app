fe5644ed3665db30582d7971bc5b129984736d16
JOANITA-51 <nakityojoanita51@gmail.com>
2021-10-17 19:18:08 +0300

    putting our state at the top level such that we use it in any of the components

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,10 +1,31 @@
+import {useState} from 'react'
 import Header from "./components/Header"
-import Tasks from "./components/tasks"
+import Tasks from "./components/Tasks"
 const App = () => {
+  const [tasks, setTasks] = useState([ //seTasks changes any part of the state
+    {
+        id: 1, 
+        text: 'Doctors Appointment',
+        day:'Feb 5th at 3:30pm',
+        reminder: true, 
+    } , 
+    {
+      id: 2, 
+      text: 'Meeting at school',
+      day:'Feb 6th at 5:30pm',
+      reminder: true, 
+  } ,
+  {
+      id: 3, 
+      text: 'Food Shopping',
+      day:'Feb 7th at 2:30pm',
+      reminder: false, 
+  } ,
+])
   return (
     <div className="container">
       <Header />
-      <Tasks/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- react-task-tracker/src/components/tasks.js
+++ react-task-tracker/src/components/tasks.js
@@ -1,32 +1,12 @@
-import {useState} from 'react'
-const tasks = () => {
-    const [Tasks, setTasks] = useState([ //seTasks changes any part of the state
-        {
-            id: 1, 
-            text: 'Doctors Appointment',
-            day:'Feb 5th at 3:30pm',
-            reminder: true, 
-        } , 
-        {
-          id: 2, 
-          text: 'Meeting at school',
-          day:'Feb 6th at 5:30pm',
-          reminder: true, 
-      } ,
-      {
-          id: 3, 
-          text: 'Food Shopping',
-          day:'Feb 7th at 2:30pm',
-          reminder: false, 
-      } ,
-    ])
+const Tasks = ({tasks}) => {
+
     return (
         <>
-          {Task.map((T)=>(
-              <h3 key={T.id}>{T.text}</h3>
+          {tasks.map((task)=>(
+              <h3 key={task.id}>{task.text}</h3>
           ))}   
         </>
     )
 }
 
-export default tasks
+ export default Tasks
