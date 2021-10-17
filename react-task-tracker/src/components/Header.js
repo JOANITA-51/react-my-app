a7c981b278c0e93697af65e976c109dfce1123f5
JOANITA-51 <nakityojoanita51@gmail.com>
2021-10-17 22:50:41 +0300

    changing the color too

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -6,7 +6,8 @@ const Header = ({title, onAdd, showAdd }) => {
     return (
         <header className='header'>
             <h1 >{title}</h1>
-            <Button color= 'green' text = {showAdd?'close':'Add'} onClick={onAdd}/>
+            <Button color= {showAdd?'red':'green'}
+             text = {showAdd?'close':'Add'} onClick={onAdd}/>
         </header>
     )
 }
