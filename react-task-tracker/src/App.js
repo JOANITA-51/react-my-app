fb1d73c9cbb6f7edf54c63199278f185bb07e6b4
JOANITA-51 <nakityojoanita51@gmail.com>
2021-10-17 20:40:04 +0300

    Enabling the delete event / function have action to the state by retutning the task id when the icon is clicked

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -22,10 +22,14 @@ const App = () => {
       reminder: false, 
   } ,
 ])
+
+const deleteTask = (id) =>{
+  console.log('delete', id)
+}
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
