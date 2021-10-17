571e9c480c1ca5249b951bc3deddce2f9917d85e
JOANITA-51 <nakityojoanita51@gmail.com>
2021-10-17 12:40:30 +0300

    Including the component to be part of the state

--- react-task-tracker/src/components/tasks.js
+++ react-task-tracker/src/components/tasks.js
@@ -1,24 +1,25 @@
-const Task = [
-  {
-      id: 1, 
-      text: 'Doctors Appointment',
-      day:'Feb 5th at 3:30pm',
-      reminder: true, 
-  } , 
-  {
-    id: 2, 
-    text: 'Meeting at school',
-    day:'Feb 6th at 5:30pm',
-    reminder: true, 
-} ,
-{
-    id: 3, 
-    text: 'Food Shopping',
-    day:'Feb 7th at 2:30pm',
-    reminder: false, 
-} ,
-]
+import {useState} from 'react'
 const tasks = () => {
+    const [Tasks, setTasks] = useState([ //seTasks changes any part of the state
+        {
+            id: 1, 
+            text: 'Doctors Appointment',
+            day:'Feb 5th at 3:30pm',
+            reminder: true, 
+        } , 
+        {
+          id: 2, 
+          text: 'Meeting at school',
+          day:'Feb 6th at 5:30pm',
+          reminder: true, 
+      } ,
+      {
+          id: 3, 
+          text: 'Food Shopping',
+          day:'Feb 7th at 2:30pm',
+          reminder: false, 
+      } ,
+    ])
     return (
         <>
           {Task.map((T)=>(
