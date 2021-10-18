597dff730db5a6e17cdb9fd220611a3b23243993
JOANITA-51 <nakityojoanita51@gmail.com>
2021-10-18 11:50:03 +0300

    mtd 2 of conditional rendering using a ternary opertor

--- react-task-tracker/src/components/Accordion.js
+++ react-task-tracker/src/components/Accordion.js
@@ -3,9 +3,11 @@ const Accordion = () => {
     const [isToggled, setIsToggled]= useState(false ) //false enables sth to be hidden
     return (
         <div>
-        {isToggled && 
+        {
+            isToggled? <h3>show me</h3> :null
+        /* {isToggled && 
            <h3>show me</h3> 
-        }
+        } */}
            <button 
            onClick = {()=> setIsToggled(!isToggled)}
            >Toggle</button>
